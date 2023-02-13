4b6aa87d77fd17e5bf4a162725a4a779eee8da92
LorenzovandenDungen <2130766@talnet.nl>
2023-02-13 12:20:12 +0100

    Yup

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='sticky top-0 w-full left-0'>
+        <h1 className='text-3xl sm:text-6xl'>Security Awareness training</h1>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Login from '@/components/Login'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +14,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-Lorenzo
-        </div>
+      <Login />
+      <i className="fa-brands fa-github"></i>
+      </div>
   )
 }
