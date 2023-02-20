19150fe5a682f2ecb02b201c5f25f6b1b848eaa6
LorenzovandenDungen <2130766@talnet.nl>
2023-02-20 13:11:55 +0100

    save

--- components/UserDashboard.js
+++ components/UserDashboard.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { useAuth } from '../context/AuthContext'
 
-function UserDashboard(props) {
-    return (
-        <div>UserDashboard</div>
-    );
+export default function UserDashboard() {
+const { userInfo } = useAuth()
+const [addTodo, setAddTodo] = useState(false)
+const [todo, setTodo] = useState('')
+return (
+    <div className='w-full max-w-[65ch] text-xs sm:text-sm mx-auto flex flex-col gap-3 sm:gap-5'>
+        <input 
+            type="text" 
+            placeholder="Enter todo" 
+            value={todo} 
+            onChange={(e) => setTodo(e.target.value)}
+            className="outline-none p-3 text-base sm:text-lg text-slate-900"
+        />
+        {userInfo && (
+            <>
+                {/* Your code here */}
+            </>
+        )}
+        <button 
+            className="text-cyan-300 border border-solid border-cyan-300 py-2 text-center uppercase text-lg duration-300 hover:opacity-30"
+        >
+            ADD TODO
+        </button>
+    </div>
+)
+
+
 }
-
-export default UserDashboard;
