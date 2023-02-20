e50c98cf7061b31ff2b2a0ed2b6b0476b1c62165
LorenzovandenDungen <2130766@talnet.nl>
2023-02-20 12:37:22 +0100

    save

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Modal from './Modal'
 
 export default function Header() {
-  return (
-    <div className='sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white'>
-        <h1 className='text-3xl select-none sm:text-6xl'>Security Awareness Training</h1>
-        <i className="fa-solid fa-user text-xl duration-300 hover:opacity-40 cursor-pointer sm:text-3xl"></i>
-    </div>
-  )
-}
+    const [openModal, setOpenModal] = useState(false); 
+
+    return(
+      <>
+        {openModal && <Modal setOpenModal={setOpenModal} />}
+        <div className="sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white">
+          <hl className="text-3x1 select-none sm:text-6x1">TODO LIST</hl>
+          <i onClick={() => setOpenModal(true)} className="fa-solid fa-user text-xl duration-300 hover:opacity-40 cursor-pointer sm:text-3x1"></i>
+          </div>
+          </> 
+          )
+        }
--- components/Modal.js
+++ components/Modal.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import  ReactDom from 'react-dom'
 
 export default function Modal(props) {
-  const { setOpenModal } = props;
+     const { setOpenModal } = props;
+     const [_document, set_document] = useState(null)
 
-  return (
-    <div className='fixed inset-0 bg-white text-slate-900 flex flex-col'>
-      <div className='flex items-center justify-between border-b border-solid border-slate-900 p-4'>
-        <h1>MENU</h1>
-        <i className="fa-solid fa-xmark"></i>
-      </div>
-      <div className='p-4 flex flex-col gap-3'>
-        <h2>Logout</h2>
-      </div>
-    </div>
+     useEffect(() => {
+      setDocument(document)
+  }, [])
+  
+  if (!_document) return null;
+  
+  return ReactDOM.createPortal(
+      <div className='fixed inset-0 bg-white text-slate-900 flex flex-col'>
+          <div className="flex items-center justify-end border-b border-solid border-slate-900 p-4"> 
+              <h1>MENU</h1> 
+              <i className="fa-solid fa-xmark" onClick={() => setOpenModal(false)}></i>  
+          </div>
+          <div className="p-4 flex flex-col gap-3">
+              <h2>Logout</h2>
+          </div>
+      </div>,
+      _document.getElementById('portal')
   )
 }
