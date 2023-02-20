abedde6b8e56c96a4e6c52ff78abfe728f796889
LorenzovandenDungen <2130766@talnet.nl>
2023-02-20 11:08:21 +0100

    save

---
+++ components/AdminDashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AdminDashboard extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Admin Dashboard</h1>
+
+            </div>
+        )
+    }
+}
+
+export default AdminDashboard;
