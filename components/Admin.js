cd8484f7cd6398742b3bb0072eb8291fd1737a15
LorenzovandenDungen <2130766@talnet.nl>
2023-02-20 14:13:26 +0100

    save

--- components/Admin.js
+++ components/Admin.js
@@ -1,26 +1,35 @@
 import React, { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 
+// Here you can import the Invites, Users and Trainings components
+import Invites from // insert path here
+import Users from // insert path here
+import Trainings from // insert path here
+
 const Admin = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleLogin = () => {
-      setIsLoggedIn(true);
-  };
+    const handleLogin = () => {
+        setIsLoggedIn(true);
+    };
+​
+    if (isLoggedIn) {
+        return <Redirect to="/dashboard" />;
+    }
 
-  if (isLoggedIn) {
-      return <Redirect to="/dashboard" />;
-  }
-
-  return (
-      <div>
-        <h2>Admin Login</h2>
-        <input type="text" placeholder="username"/>
-        <input type="password" placeholder="password"/>
-        <button onClick={handleLogin}>Login</button>
-        <Link to="/register"><p>Register</p></Link>  
-      </div>
-  );
+    return (
+        <div>
+            <h2>Admin Login</h2>
+            <input type="text" placeholder="username"/>
+            <input type="password" placeholder="password"/>
+            <button onClick={handleLogin}>Login</button>
+            <Link to="/register"><p>Register</p></Link>  
+            <Invites />
+            <Users />
+            <Trainings />
+            <Grades />
+        </div>
+    );
 };
 
 export default Admin;
